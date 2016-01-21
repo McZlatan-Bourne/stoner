22d49a142ee3bc9229d83993f4b26e863053ae55
McZlatan-Bourne <bournethesonofthegun@gmail.com>
2016-01-21 12:24:56 +0100

    Update fileHelper.py

--- stoner/fileHelper.py
+++ stoner/fileHelper.py
@@ -3,7 +3,7 @@
 import os
 
 def checkfile(path):
-				"""Confirms file to be uploaded."""
-				if os.path.isfile(path) == True and os.path.exists(path) == True:
-								return str(path)
-				else:return False
+	"""Confirms file to be uploaded."""
+		if os.path.isfile(path) == True and os.path.exists(path) == True:
+			return str(path)
+		else:return False
